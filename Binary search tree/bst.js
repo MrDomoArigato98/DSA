16284944b4f5d78b9f8c28665c21992fd4a0be46
MrDomoArigato98 <domdob98@gmail.com>
2025-03-18 12:19:02 +0000

    Binary search tree insert and delete

---
+++ Binary search tree/bst.js
@@ -0,0 +1,159 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(array = []) {
+    array = array.sort(function (a, b) {
+      return a - b;
+    });
+
+    array = [...new Set(array)];
+    this.array = array;
+    this.root = this.buildTree(array);
+  }
+
+  buildTree(array) {
+    if (array.length === 0) {
+      return null;
+    }
+    const mid = Math.floor(array.length / 2);
+
+    let root = new Node(array[mid]);
+
+    root.left = this.buildTree(array.slice(0, mid));
+    root.right = this.buildTree(array.slice(mid + 1));
+
+    return root;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    let current = this.root;
+
+    if (current === null) {
+      return newNode;
+    }
+
+    let parent = null;
+
+    while (current !== null) {
+      parent = current;
+      if (current.data > value) {
+        current = current.left;
+      } else if (current.data < value) {
+        current = current.right;
+      } else return this.current;
+    }
+
+    if (parent.data < value) {
+      parent.right = newNode;
+    } else {
+      parent.left = newNode;
+    }
+    return this.root;
+  }
+
+  deleteItem(value) {
+    let current = this.root;
+    let parent = null;
+
+    while (current !== null && current.data !== value) {
+      parent = current; //Points to the parent of the value to be deleted
+      if (value < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+
+    if (current === null) {
+      // If the value isn't found in the tree
+      return this.root;
+    }
+
+    // If node has one child.
+    // 1. Must get the child of the node I'm deleting.
+    // 2. Move it up to take it's place
+    // 3. Point the parent to the replacement
+    if (current.left === null || current.right === null) {
+      let child = null;
+
+      if (current.left === null) {
+        console.log(`Key ${value} has one child on the right. Moving child up`);
+        child = current.right;
+      } else {
+        console.log(`Key ${value} has one child on the left. Moving child up`);
+        child = current.left;
+      }
+      if (current === parent.left) {
+        parent.left = child;
+      } else {
+        parent.right = child;
+      }
+    }
+
+    //If node has no children. Just remove the leaf.
+    //1. Check if current is parent's left or right, and set it to null
+    if (current.left === null && current.right === null) {
+      console.log(`Key ${value} is a leaf node. Removing.`);
+      if (current === parent.left) {
+        parent.left = null;
+      } else {
+        parent.right = null;
+      }
+    }
+
+    //If node has two children.
+    //1. Need to find an in-order successor. Traverse LEFT in the right subtree to find the smallest node.
+    if (current.left !== null && current.right !== null) {
+      console.log(`Key ${value} has two child nodes.`);
+      let parentOfSuccessor = null;
+      let successor = current.right;
+
+      while (successor.left !== null) {
+        //As long as there are nodes to the left, there's a smaller value for the successor.
+        parentOfSuccessor = successor; //Set the parent
+        successor = successor.left; //Keep traversing left
+      }
+      console.log(parentOfSuccessor);
+      console.log(successor);
+
+      if (parentOfSuccessor !== null) {
+        //This means that the successor is not the immediate right child of current. (The while loop went through and traversed)
+        parentOfSuccessor.left = successor.right;
+      } else {
+        current.right = successor.right;
+      }
+      current.data = successor.data;
+    }
+  }
+
+  prettyPrint(node, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false
+      );
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  }
+}
+
+const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+const tree = new Tree(array);
+
+// tree.deleteItem(67);
+tree.prettyPrint(tree.root);
