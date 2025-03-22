82647d5920857c9e352df15cf3248d7f980482ca
MrDomoArigato98 <domdob98@gmail.com>
2025-03-22 10:58:34 +0000

    Completes Knight-travails

--- Knight Travails/knight-travails.js
+++ Knight Travails/knight-travails.js
@@ -1,30 +1,30 @@
 function knightMoves(start = [], destination = []) {
   //Push the first element to the beginning of the queue;
   const queue = [];
-
   queue.push(start);
-  console.table(queue);
 
-  const visited = new Set();
+  const visited = new Map();
+  visited.set(start.toString(), null);
 
   while (queue.length) {
-    let index = queue.pop(); // Pop the first element from the queue
-    visited.add(index); // Add the index to the visited set
-
+    let index = queue.shift(); // Pop the first element from the queue
     const neighbours = calculateNeighbours(index); // Calculates the list of possible neighbours from the index
-    console.log("Neighbours are:");
-    console.table(neighbours); // List out the neighbours
+    console.log(index);
+
+    if (index[0] === destination[0] && index[1] === destination[1]) {
+      //If they match, we have to backtrack the number of steps
+      return backtrack(visited, destination);
+    }
 
     for (let neighbour of neighbours) {
-      //For each possible neighbour
-      if (neighbour[0] == destination[0] && neighbour[1] == destination[1]) {
-        // Check if neighbour matches destination
-        return visited; // Not sure what to return here, the visited set? Probably
+      //For each neighbour of the index
+      if (!visited.has(neighbour.toString())) {
+        visited.set(neighbour.toString(), index); //Set the Key to the string of [x,y] coordinates, and the value to it's parent
+        queue.push(neighbour);
       }
     }
   }
 }
-
 function calculateNeighbours(index = []) {
   // Start off with [0,0]
 
@@ -51,10 +51,20 @@ function calculateNeighbours(index = []) {
 
   return validMoves;
 }
+
+function backtrack(visited, destination) {
+  const path = [];
+  let current = destination;
+
+  while (current) {
+    path.push(current); //Add the path to current. This begins with the destination.
+    current = visited.get(current.toString()); //Set current, to the value of it's parent. The map contains contains [Child x,y : parent x,y]
+  }
+  return path.reverse(); //Return the reverse of the list, as we're backtracking from the destination.
+}
+
 // Your task is to build a function knightMoves that shows the shortest possible way to get from one square to another
 // by outputting all squares the knight will stop on along the way.
 // Start at 0,0 wants to move to 1,2 so the answer to this one is == [[0,0], [1,2]]
-let a = knightMoves([3, 2], [7, 7]);
 
-console.log("Here's the end:");
-console.table(a);
+console.table(knightMoves([0,0], [7,7]));
