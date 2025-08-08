cc540a847211c66c2af45dec386dd51d2943c98f
MrDomoArigato98 <domdob98@gmail.com>
2025-08-08 13:01:56 +0100

    Insertion sort

---
+++ Sorting Algorithms/Insertion Sort/solution.js
@@ -0,0 +1,14 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let curr = arr[i];
+    let j;
+
+    for (j = i - 1; j >= 0 && arr[j] > curr; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = curr;
+  }
+  return arr;
+}
+
+console.log(insertionSort([1,2,4,11,222,33,1,2,3,6]));
